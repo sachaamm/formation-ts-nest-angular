b9ad7fd27f2d111b4bbc921ec24e0fb63825abde
alexandre amiel <alexisamielle@gmail.com>
2025-06-25 18:10:11 +0200

    NestJs v3 Fix Import for AnimalRepo

--- zoo-backend/src/animaux/animaux.module.ts
+++ zoo-backend/src/animaux/animaux.module.ts
@@ -1,9 +1,12 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AnimauxController } from './animaux.controller';
 import { AnimauxService } from './animaux.service';
+import { Animal } from './entities/animal.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([Animal])],
   controllers: [AnimauxController],
-  providers: [AnimauxService]
+  providers: [AnimauxService],
 })
 export class AnimauxModule {}
