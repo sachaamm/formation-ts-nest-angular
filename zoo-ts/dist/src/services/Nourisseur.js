ec854fce53c28e2b8d9b3e67fe59ddaa29aff776
alexandre amiel <alexisamielle@gmail.com>
2025-06-26 11:43:59 +0200

    Suppression du dossier dist

--- zoo-ts/dist/src/services/Nourisseur.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Nourrisseur = void 0;
-class Nourrisseur {
-    nourrir(animal) {
-        animal.nourrir();
-    }
-}
-exports.Nourrisseur = Nourrisseur;
